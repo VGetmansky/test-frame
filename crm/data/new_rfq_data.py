caecfc108125f1372b10bf4d85e7cdf025295d05
vgetmansky <vladislav.getmanskiy@zimad.com>
2018-10-24 16:14:32 +0300

    id's for new RFQ

---
+++ crm/data/new_rfq_data.py
@@ -0,0 +1,31 @@
+
+#   ----------  Information / Address   ----------
+
+rfq_number_id = "rfq-number"
+
+rfq_search_acoount_id = "rfq-search-account"
+rfq_search_contact_id = "rfq-search-contact"
+
+rfq_search_value_id = "search-value"
+rfq_account_search_button_id = "search-button"
+
+rfq_priority_id = "rfq-priority"
+
+#   ----------  Details  ----------
+
+rfq_customer_quote_id = "rfq-customer-quote"
+expiration_date_id = "rfq-experation-date"
+received_via_id = "rfq-received-via"
+rfq_terms_of_sale_id = "rfq-tsale"
+rfq_terms_of_delivery_id = "rfq-tdelivery"
+rfq_place_of_delivery_id = "rfq-pdelivery"
+rfq_description_id = "rfq-description"
+rfq_territory_id = "rfq-territory"
+rfq_assigned_to_id = "rfq-assigned"
+rfq_aircraft_id = "rfq-aircraft"
+rfq_location_id = "rfq-location"
+
+#   ----------  Parts list   ----------
+
+rfq_status_id = "rfq-status"
+rfq_created_status = "//li[contains(., 'Created')]"
